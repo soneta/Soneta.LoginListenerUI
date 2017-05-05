cfde53856edc83fad570be3ae3adb36c50d06998
Kamil Dudzinski <kamil.dudzinski@enova.pl>
2017-05-05 11:28:58 +0200

    Initial commit

---
+++ enova365.TwoFactorAuth/Rejestracje.cs
@@ -0,0 +1,7 @@
+﻿using Soneta.Business.UI;
+
+[assembly: FolderView("enova365.TwoFactorAuth",
+    Priority = 1000,
+    Description = "Dodatek dla enova365",
+    BrickColor = FolderViewAttribute.YellowBrick
+)]
---
+++ enova365.TwoFactorAuth/TwoFactor.cs
@@ -0,0 +1,28 @@
+﻿using Soneta.Business;
+using Soneta.Business.App;
+
+[assembly: Service(typeof(ILoginListenerUI), typeof(enova365.TwoFactorAuth.MyListener), Priority = 200)]
+
+namespace enova365.TwoFactorAuth
+{
+    public class TwoFactor
+    {
+        [Context]
+        public Context Context { get; set; }
+
+        public string Komunikat
+        {
+            get { return "przykład lalaal"; }
+        }
+    }
+
+    public class MyListener : ILoginListenerUI
+    {
+        Soneta.Types.Currency c1 = Soneta.Types.Currency.Zero;
+
+        public void AfterLoginResult(AfterLoginResultArgs args)
+        {
+            args.Values.Add( new TwoFactor());
+        }
+    }
+}
